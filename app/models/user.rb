99a0f133b1431aaa7048f76629d900ffef25e216
koseki3104 <satoshi4500sports@icloud.com>
2023-07-02 16:37:58 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
 
 validates :nickname, presence: true, length: { maximum: 40 }
 validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+\z/ }
-validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
-validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
+validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ヶー]+\z/ }
+validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ヶー]+\z/ }
 validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
 validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
 validates :birth_date, presence: true
