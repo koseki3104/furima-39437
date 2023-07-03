dfeafb02dcc9d1676d756793828c8cc32da0c9da
koseki3104 <satoshi4500sports@icloud.com>
2023-07-03 12:49:17 +0900

    UserモデルとItemsモデルのアソシエーション定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,8 @@
 class Article < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+
+  belongs_to :user 
+
   belongs_to :category
   belongs_to :condition
   belongs_to :shipping_cost
