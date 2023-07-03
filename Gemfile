0688498bd639622af3b9c6eb2d714124ad71fc94
koseki3104 <satoshi4500sports@icloud.com>
2023-07-03 14:10:55 +0900

    ActiveStrogeを用いて画像投稿を可能にする

--- Gemfile
+++ Gemfile
@@ -83,3 +83,6 @@ end
 
 gem 'devise'
 gem 'active_hash'
+
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,9 @@
 class Article < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
-  belongs_to :user 
+  belongs_to :user
+  
+  has_one_attached :image 
 
   belongs_to :category
   belongs_to :condition
