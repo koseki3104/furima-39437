48e47102f84d063acd42ff59bc765f6bcdafb697
koseki3104 <satoshi4500sports@icloud.com>
2023-07-03 15:29:35 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
  has_many :items       
 
 validates :nickname, presence: true, length: { maximum: 40 }
-validates :password, format: { with: /\A(?=.?[a-z])(?=.?[\d])[a-z\d]+\z/i }
+validates :password, format: { with: /(?=.*[a-z])(?=.*\d)[a-z\d]+\z/i }
 validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
 validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
 validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
