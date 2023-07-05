558ef817a9649bf7f043fe92a2cbf73f646dec72
koseki3104 <satoshi4500sports@icloud.com>
2023-07-05 15:07:45 +0900

    コントローラーの記述を省略

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:edit, :show, :update,:destroy]
+  before_action :set_item, only: [:edit, :show, :update,:destroy ]
   before_action :move_to_edit_index, only: [:edit]
 
   def index
