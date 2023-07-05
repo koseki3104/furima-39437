4b1003eeacbd1b22fbb95141feeac1f1600837f2
koseki3104 <satoshi4500sports@icloud.com>
2023-07-05 10:36:45 +0900

    コントローラーの記述省略

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,15 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index,:show]
-  before_action :move_to_edit_index, only: [:edit]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:edit, :show, :update]
+  before_action :move_to_edit_index, only: [:edit]
 
   def index
-   @items = Item.all.order("created_at DESC")
-  end  
+    @items = Item.all.order("created_at DESC")
+  end
 
   def new
     @item = Item.new
-  end  
+  end
 
   def show
   end
@@ -37,11 +37,10 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image,:product_name,:product_description,:category_id,:condition_id,:shipping_cost_id,:prefecture_id,:shipping_duration_id,:price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :product_name, :product_description, :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_duration_id, :price).merge(user_id: current_user.id)
   end
 
   def move_to_edit_index
-    @item = Item.find(params[:id])
     unless current_user == @item.user
       redirect_to root_path
     end
