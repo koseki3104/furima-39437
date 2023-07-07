f83020ff076460e80e75d3a804272876c3adb620
koseki3104 <satoshi4500sports@icloud.com>
2023-07-07 15:38:17 +0900

    アソシエーションの定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
- has_many :items       
+      
+ has_many :items
+ has_many :orders
 
 validates :nickname, presence: true, length: { maximum: 40 }
 validates :password, format: { with: /(?=.*[a-z])(?=.*\d)[a-z\d]+\z/i }
