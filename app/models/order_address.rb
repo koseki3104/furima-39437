e6b051b7c231fc1295a5c508283528f75fb6396e
koseki3104 <satoshi4500sports@icloud.com>
2023-07-10 16:09:30 +0900

    バリデーションの修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,14 +1,15 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id ,:postal_code, :prefecture_id, :city, :address_line1,:address_line2, :phone_number
+  attr_accessor :user_id, :item_id ,:postal_code, :prefecture_id, :city, :address_line1,:address_line2, :phone_number, :token
 
   validates :user_id, presence: true
   validates :item_id, presence: true
   validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/, message: "is invalid. Should be in the format XXX-XXXX" }
-  validates :prefecture_id, presence: true
+  validates :prefecture_id, numericality: { other_than: 1 } 
   validates :city, presence: true
   validates :address_line1, presence: true
   validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/, message: "is invalid. Only allows 10 to 11 digits." }
+  validates :token, presence: true
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
