903e0181d4905ace7c58db0874c32f5d7f916db1
koseki3104 <satoshi4500sports@icloud.com>
2023-07-11 12:58:51 +0900

    テストコードの変更

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,14 +1,17 @@
 FactoryBot.define do
   factory :order_address do
+    association :user, factory: :user
+    association :item, factory: :item
+    postal_code { Faker::Number.unique.number(digits: 3).to_s + '-' + Faker::Number.unique.number(digits: 4).to_s }
+    prefecture_id { Faker::Number.between(from: 2, to: 47) }
+    city { Faker::Address.city }
+    address_line1 { Faker::Address.street_address }
+    phone_number { Faker::Number.number(digits: 10) }
+    token { "dummy_token" }
 
-user_id { Faker::Number.unique.between(from: 1, to: 100) }
-item_id { Faker::Number.unique.between(from: 1, to: 100) }
-postal_code { Faker::Number.unique.number(digits: 3).to_s + '-' + Faker::Number.unique.number(digits: 4).to_s }
-prefecture_id { Faker::Number.between(from: 2, to: 47) }
-city { Faker::Address.city }
-address_line1 { Faker::Address.street_address }
-phone_number { Faker::Number.number(digits: 10) }
-token { "dummy_token" }
-    
+    after(:build) do |order_address|
+      order_address.user = FactoryBot.create(:user)
+      order_address.item = FactoryBot.create(:item)
+    end
   end
 end
