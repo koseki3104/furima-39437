ae17ddacad46b3226d1dd448e8853a8d40e9d0a9
koseki3104 <satoshi4500sports@icloud.com>
2023-07-11 15:02:59 +0900

    リファクタリングの実装

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,15 +1,17 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id ,:postal_code, :prefecture_id, :city, :address_line1,:address_line2, :phone_number, :token
+  attr_accessor :user_id, :item_id ,:postal_code, :prefecture_id, :city, :address_line1, :address_line2, :phone_number, :token
 
-  validates :user_id, presence: true
-  validates :item_id, presence: true
-  validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/, message: "is invalid. Should be in the format XXX-XXXX" }
-  validates :prefecture_id, numericality: { other_than: 1 } 
-  validates :city, presence: true
-  validates :address_line1, presence: true
-  validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/, message: "is invalid. Only allows 10 to 11 digits." }
-  validates :token, presence: true
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: "is invalid. Should be in the format XXX-XXXX" }
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :city
+    validates :address_line1
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is invalid. Only allows 10 to 11 digits." }
+    validates :token
+  end
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
