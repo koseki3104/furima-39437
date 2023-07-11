5a0e42087318efca8f77faf09bc0ddbbce6d3d6c
koseki3104 <satoshi4500sports@icloud.com>
2023-07-11 16:10:35 +0900

    アソシエーションの削除

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,17 +1,13 @@
 FactoryBot.define do
   factory :order_address do
-    association :user, factory: :user
-    association :item, factory: :item
+
     postal_code { Faker::Number.unique.number(digits: 3).to_s + '-' + Faker::Number.unique.number(digits: 4).to_s }
     prefecture_id { Faker::Number.between(from: 2, to: 47) }
     city { Faker::Address.city }
     address_line1 { Faker::Address.street_address }
+    address_line2 { Faker::Address.street_address }
     phone_number { Faker::Number.number(digits: 10) }
     token { "dummy_token" }
 
-    after(:build) do |order_address|
-      order_address.user = FactoryBot.create(:user)
-      order_address.item = FactoryBot.create(:item)
-    end
   end
 end
