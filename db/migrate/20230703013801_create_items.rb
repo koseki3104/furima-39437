602b49876fc96428e9b02df3a615de89105c6b62
koseki3104 <satoshi4500sports@icloud.com>
2023-07-12 20:49:15 +0900

    記述の短縮

--- db/migrate/20230703013801_create_items.rb
+++ db/migrate/20230703013801_create_items.rb
@@ -2,8 +2,8 @@ class CreateItems < ActiveRecord::Migration[7.0]
   def change
     create_table :items do |t|
 
-      t.string :product_name,                null: false, default: ""
-      t.text :product_description,           null: false, default: ""
+      t.string :product_name,                null: false
+      t.text :product_description,           null: false
       t.integer :category_id,                null: false
       t.integer :condition_id,               null: false 
       t.integer :shipping_cost_id,           null: false
